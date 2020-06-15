4a9d107e97d486b4f3cb50dbee29d8d61f350b49
Momo Ramadori <momoramadori8@gmail.com>
2020-06-15 14:42:54 +0200

    Done!

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
  <!-- esempio saluto terminale <?php echo 'ciao ' . $argv[1] . ' ' . $argv[2] ?>  -->
 
+<?php 
+    $sentence = 'Benvenuto nel lato oscuro! Sono Darth Vader e sono il tuo fottuto padre!';
+    $badword = $_GET['badword'];
+    $badword_center = substr($badword,1,4);
+    $censure = '***';
+    $censured_sentence = str_replace($badword_center , $censure, $sentence);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> <?php echo $censured_sentence ?> </p>
+    <p> <?php echo strlen($sentence) ?> </p> 
+</body>
+</html>
